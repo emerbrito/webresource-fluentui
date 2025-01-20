1d371aa186e0114c2b9ca2b80a3d4514f171fbbc
Tomislav Sokac <tsokac@practicepro365.com>
2025-01-20 19:42:34 +0000

    feat: msal authentication

---
+++ src/authConfig.ts
@@ -0,0 +1,14 @@
+export const msalConfig = {
+  auth: {
+    clientId: import.meta.env.VITE_AZURE_CLIENT_ID,
+    authority: `https://login.microsoft.com/${import.meta.env.VITE_AZURE_TENANT_ID}`
+  },
+  cache: {
+    cacheLocation: 'localStorage',
+    storeAuthStateInCookie: true
+  }
+}
+
+export const authRequest = {
+  scopes: ['User.read']
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { MsalAuthenticationTemplate, MsalProvider } from '@azure/msal-react'
+import { InteractionType, PublicClientApplication } from '@azure/msal-browser'
+import { msalConfig, authRequest } from './authConfig.ts'
 import { FluentProvider, webLightTheme } from '@fluentui/react-components'
 import { AppError } from './infrastructure/AppError.tsx'
 import ErrorBoundary from './infrastructure/ErrorBoundary.tsx'
 import App from './App.tsx'
 import './index.css'
 
+const msalInstance = new PublicClientApplication(msalConfig)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary fallback={<AppError />}>
-      <FluentProvider theme={webLightTheme} style={{ height: '100%' }}>
-        <div className="h-full flex flex-col p-4 gap-4">
-          <App />
-        </div>
-      </FluentProvider>
-    </ErrorBoundary>
-  </StrictMode>,
+    <MsalProvider instance={msalInstance}>
+      <MsalAuthenticationTemplate interactionType={InteractionType.Popup} authenticationRequest={authRequest}>
+        <ErrorBoundary fallback={<AppError />}>
+          <FluentProvider theme={webLightTheme} style={{ height: '100%' }}>
+            <div className='h-full flex flex-col p-4 gap-4'>
+              <App />
+            </div>
+          </FluentProvider>
+        </ErrorBoundary>
+      </MsalAuthenticationTemplate>
+    </MsalProvider>
+  </StrictMode>
 )
