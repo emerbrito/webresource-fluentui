68e8d6633f494aec4400d0e6b5d7820222736e19
Emerson Brito <ebrito@practicepro365.com>
2025-04-09 02:04:16 -0400

    Use variables instead of hardcoded values

--- src/services/msal-config.ts
+++ src/services/msal-config.ts
@@ -2,8 +2,8 @@ import { Configuration } from "@azure/msal-browser";
 
 export const msalConfig: Configuration = {
     auth: {
-      clientId: "035672f0-12d2-4a6e-a717-b6c98841b4d3", // from Azure AD app registration
-      authority: "https://login.microsoftonline.com/practicepro365.com",
+      clientId: import.meta.env.VITE_AZURE_CLIENT_ID, // From .env.local
+      authority: `https://login.microsoftonline.com/${import.meta.env.VITE_AZURE_TENANT_ID}`,
       redirectUri: "http://localhost:5173", // or your deployed app's URL
     },
     cache: {
